d84ead546a477eae834332473fe727b32b1b597b
Christophe Schmit <ge69zat@mytum.de>
2024-05-28 01:36:00 +0200

    edited rendering

--- diffuser/utils/rendering.py
+++ diffuser/utils/rendering.py
@@ -1,50 +1,57 @@
 import os
-import numpy as np
+import pdb
+
+# import mujoco_py as mjc
+import warnings
+
 import einops
+import gymnasium as gym
 import imageio
 import matplotlib.pyplot as plt
+import numpy as np
 from matplotlib.colors import ListedColormap
-import gymnasium as gym
-#import mujoco_py as mjc
-import warnings
-import pdb
+
+from diffuser.datasets.d4rl import load_environment
 
 from .arrays import to_np
 from .video import save_video, save_videos
 
-from diffuser.datasets.d4rl import load_environment
+# -----------------------------------------------------------------------------#
+# ------------------------------- helper structs ------------------------------#
+# -----------------------------------------------------------------------------#
 
-#-----------------------------------------------------------------------------#
-#------------------------------- helper structs ------------------------------#
-#-----------------------------------------------------------------------------#
 
 def env_map(env_name):
-    '''
-        map D4RL dataset names to custom fully-observed
-        variants for rendering
-    '''
-    if 'halfcheetah' in env_name:
-        return 'HalfCheetahFullObs-v2'
-    elif 'hopper' in env_name:
-        return 'HopperFullObs-v2'
-    elif 'walker2d' in env_name:
-        return 'Walker2dFullObs-v2'
+    """
+    map D4RL dataset names to custom fully-observed
+    variants for rendering
+    """
+    if "halfcheetah" in env_name:
+        return "HalfCheetahFullObs-v2"
+    elif "hopper" in env_name:
+        return "HopperFullObs-v2"
+    elif "walker2d" in env_name:
+        return "Walker2dFullObs-v2"
     else:
         return env_name
 
-#-----------------------------------------------------------------------------#
-#------------------------------ helper functions -----------------------------#
-#-----------------------------------------------------------------------------#
+
+# -----------------------------------------------------------------------------#
+# ------------------------------ helper functions -----------------------------#
+# -----------------------------------------------------------------------------#
+
 
 def atmost_2d(x):
     while x.ndim > 2:
         x = x.squeeze(0)
     return x
 
+
 def zipsafe(*args):
     length = len(args[0])
     assert all([len(a) == length for a in args])
     return zip(*args)
+
 
 def zipkw(*args, **kwargs):
     nargs = len(args)
@@ -56,10 +63,12 @@ def zipkw(*args, **kwargs):
         zipped_kwargs = {k: v for k, v in zipsafe(keys, items[nargs:])}
         yield zipped_args, zipped_kwargs
 
+
 def get_image_mask(img):
     background = (img == 255).all(axis=-1, keepdims=True)
     mask = ~background.repeat(3, axis=-1)
     return mask
+
 
 def plot2img(fig, remove_margins=True):
     # https://stackoverflow.com/a/35362787/2912349
@@ -73,11 +82,12 @@ def plot2img(fig, remove_margins=True):
     canvas = FigureCanvasAgg(fig)
     canvas.draw()
     img_as_string, (width, height) = canvas.print_to_buffer()
-    return np.fromstring(img_as_string, dtype='uint8').reshape((height, width, 4))
+    return np.fromstring(img_as_string, dtype="uint8").reshape((height, width, 4))
 
-#-----------------------------------------------------------------------------#
-#---------------------------------- renderers --------------------------------#
-#-----------------------------------------------------------------------------#
+
+# -----------------------------------------------------------------------------#
+# ---------------------------------- renderers --------------------------------#
+# -----------------------------------------------------------------------------#
 
 # # class MuJoCoRenderer:
 #     '''
@@ -266,23 +276,25 @@ def plot2img(fig, remove_margins=True):
 #     def __call__(self, *args, **kwargs):
 #         return self.renders(*args, **kwargs)
 
-#-----------------------------------------------------------------------------#
-#----------------------------------- maze2d ----------------------------------#
-#-----------------------------------------------------------------------------#
+# -----------------------------------------------------------------------------#
+# ----------------------------------- maze2d ----------------------------------#
+# -----------------------------------------------------------------------------#
 
 MAZE_BOUNDS = {
-    'maze2d-umaze-v1': (0, 5, 0, 5),
-    'maze2d-medium-v1': (0, 8, 0, 8),
-    'maze2d-large-v1': (0, 9, 0, 12),
-    'PointMaze_Large-v3': (0, 9, 0, 12)
+    "maze2d-umaze-v1": (0, 5, 0, 5),
+    "maze2d-medium-v1": (0, 8, 0, 8),
+    "maze2d-large-v1": (0, 9, 0, 12),
+    "PointMaze_Large-v3": (0, 9, 0, 12),
 }
+
 
 class MazeRenderer:
 
     def __init__(self, env):
-        if type(env) is str: env = load_environment(env)
+        if type(env) is str:
+            env = load_environment(env)
         self._config = env._config
-        self._background = self._config != ' '
+        self._background = self._config != " "
         self._remove_margins = False
         self._extent = (0, 1, 1, 0)
 
@@ -290,24 +302,31 @@ class MazeRenderer:
         plt.clf()
         fig = plt.gcf()
         fig.set_size_inches(5, 5)
-        plt.imshow(self._background * .5,
-            extent=self._extent, cmap=plt.cm.binary, vmin=0, vmax=1)
+        plt.imshow(
+            np.array((self._background)) * 0.5,
+            extent=self._extent,
+            cmap=plt.cm.binary,
+            vmin=0,
+            vmax=1,
+        )
 
         path_length = len(observations)
-        colors = plt.cm.jet(np.linspace(0,1,path_length))
-        plt.plot(observations[:,1], observations[:,0], c='black', zorder=10)
-        plt.scatter(observations[:,1], observations[:,0], c=colors, zorder=20)
-        plt.axis('off')
+        colors = plt.cm.jet(np.linspace(0, 1, path_length))
+        plt.plot(observations[:, 1], observations[:, 0], c="black", zorder=10)
+        plt.scatter(observations[:, 1], observations[:, 0], c=colors, zorder=20)
+        plt.axis("off")
         plt.title(title)
         img = plot2img(fig, remove_margins=self._remove_margins)
         return img
 
     def composite(self, savepath, paths, ncol=5, **kwargs):
-        '''
-            savepath : str
-            observations : [ n_paths x horizon x 2 ]
-        '''
-        assert len(paths) % ncol == 0, 'Number of paths must be divisible by number of columns'
+        """
+        savepath : str
+        observations : [ n_paths x horizon x 2 ]
+        """
+        assert (
+            len(paths) % ncol == 0
+        ), "Number of paths must be divisible by number of columns"
 
         images = []
         for path, kw in zipkw(paths, **kwargs):
@@ -316,10 +335,12 @@ class MazeRenderer:
         images = np.stack(images, axis=0)
 
         nrow = len(images) // ncol
-        images = einops.rearrange(images,
-            '(nrow ncol) H W C -> (nrow H) (ncol W) C', nrow=nrow, ncol=ncol)
+        images = einops.rearrange(
+            images, "(nrow ncol) H W C -> (nrow H) (ncol W) C", nrow=nrow, ncol=ncol
+        )
         imageio.imsave(savepath, images)
-        print(f'Saved {len(paths)} samples to: {savepath}')
+        print(f"Saved {len(paths)} samples to: {savepath}")
+
 
 class Maze2dRenderer(MazeRenderer):
 
@@ -329,14 +350,14 @@ class Maze2dRenderer(MazeRenderer):
         self.observation_dim = np.prod(self.env.observation_space.shape)
         self.action_dim = np.prod(self.env.action_space.shape)
         self.goal = None
-        self._background = self.env.maze.maze_map == 10
+        self._background = self.env.maze.maze_map
         self._remove_margins = False
         self._extent = (0, 1, 1, 0)
 
     def renders(self, observations, conditions=None, **kwargs):
         bounds = MAZE_BOUNDS[self.env_name]
 
-        observations = observations + .5
+        observations = observations + 0.5
         if len(bounds) == 2:
             _, scale = bounds
             observations /= scale
@@ -345,33 +366,37 @@ class Maze2dRenderer(MazeRenderer):
             observations[:, 0] /= iscale
             observations[:, 1] /= jscale
         else:
-            raise RuntimeError(f'Unrecognized bounds for {self.env_name}: {bounds}')
+            raise RuntimeError(f"Unrecognized bounds for {self.env_name}: {bounds}")
 
         if conditions is not None:
             conditions /= scale
         return super().renders(observations, conditions, **kwargs)
 
-#-----------------------------------------------------------------------------#
-#---------------------------------- rollouts ---------------------------------#
-#-----------------------------------------------------------------------------#
+
+# -----------------------------------------------------------------------------#
+# ---------------------------------- rollouts ---------------------------------#
+# -----------------------------------------------------------------------------#
+
 
 def set_state(env, state):
     qpos_dim = env.sim.data.qpos.size
     qvel_dim = env.sim.data.qvel.size
     if not state.size == qpos_dim + qvel_dim:
         warnings.warn(
-            f'[ utils/rendering ] Expected state of size {qpos_dim + qvel_dim}, '
-            f'but got state of size {state.size}')
-        state = state[:qpos_dim + qvel_dim]
+            f"[ utils/rendering ] Expected state of size {qpos_dim + qvel_dim}, "
+            f"but got state of size {state.size}"
+        )
+        state = state[: qpos_dim + qvel_dim]
 
     env.set_state(state[:qpos_dim], state[qpos_dim:])
 
+
 def rollouts_from_state(env, state, actions_l):
-    rollouts = np.stack([
-        rollout_from_state(env, state, actions)
-        for actions in actions_l
-    ])
+    rollouts = np.stack(
+        [rollout_from_state(env, state, actions) for actions in actions_l]
+    )
     return rollouts
+
 
 def rollout_from_state(env, state, actions):
     qpos_dim = env.sim.data.qpos.size
@@ -382,7 +407,7 @@ def rollout_from_state(env, state, actions):
         observations.append(obs)
         if term:
             break
-    for i in range(len(observations), len(actions)+1):
+    for i in range(len(observations), len(actions) + 1):
         ## if terminated early, pad with zeros
-        observations.append( np.zeros(obs.size) )
+        observations.append(np.zeros(obs.size))
     return np.stack(observations)
