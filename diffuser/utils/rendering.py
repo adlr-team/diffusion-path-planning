attempt to fix conditioning. #TODO: doesn't seem to follow that conditioning... (issue with normalization?)

diffuser/utils/rendering.py
@@ -311,16 +311,14 @@ class MazeRenderer:
         # Define the extent to center the plot at (0, 0)
         self.extent = [-cols/2, cols/2, -rows/2, rows/2]
 
-
-        # Have to apply 0 input to get the current state
-        action = np.array([[0, 0]])
-        
         
         self.goal = self.env.unwrapped.goal
         self.starting = self.env.unwrapped.point_env.init_qpos[:2] 
+        print("The goal in rendering is", self.goal)
+        print("The starting point in rendering is", self.starting)
 
 
-    def renders(self, observations, conditions=None, title=None):
+    def renders(self, env, observations, conditions=None, title=None):
         plt.clf()
         fig = plt.gcf()
         #fig.set_size_inches(5, 5)
@@ -332,6 +330,9 @@ class MazeRenderer:
             # vmax=1,
         )
 
+        goal = env.unwrapped.goal
+        starting = env.unwrapped.point_env.init_qpos[:2] 
+
         path_length = len(observations)
         #observations = observations.reshape(len(observations), -1)
         colors = plt.cm.jet(np.linspace(0, 1, path_length))
@@ -339,11 +340,14 @@ class MazeRenderer:
         plt.scatter(observations[:, 0], observations[:, 1], c=colors, zorder=20)
         print("The last observation", observations[-1])
 
+        print("Goal in rendering:", goal)
+        print("Starting point in rendering:", starting)
+
         # Plot the goal as a red ball
         # Have to apply 0 input to get the current state
         observation_size = plt.scatter(observations[:, 0], observations[:, 1]).get_sizes()[0]
-        plt.scatter(self.goal[0], self.goal[1], c="green", marker="*", s=4*observation_size)
-        plt.scatter(self.starting[0], self.starting[1], c="black", marker="D", s=4*observation_size)
+        plt.scatter(goal[0], goal[1], c="green", marker="*", s=4*observation_size)
+        plt.scatter(starting[0], starting[1], c="black", marker="D", s=4*observation_size)
 
         #plt.scatter(observations[-1, 0], observations[-1, 1], c="red", marker="D")
         
@@ -355,7 +359,7 @@ class MazeRenderer:
         plt.show()
         return img
 
-    def composite(self, savepath, paths, ncol=1, **kwargs):
+    def composite(self, savepath, paths, env=None, ncol=1, **kwargs):
         """
         savepath : str
         observations : [ n_paths x horizon x 2 ]
@@ -368,7 +372,7 @@ class MazeRenderer:
 
         images = []
         for path, kw in zipkw(paths, **kwargs):
-            img = self.renders(*path, **kw)
+            img = self.renders(env, *path, **kw)
             images.append(img)
         images = np.stack(images, axis=0)
 
@@ -412,6 +416,7 @@ class Maze2dRenderer(MazeRenderer):
 
         self.starting = self.env.unwrapped.point_env.init_qpos[:2] 
 
+        
     # def renders(self, observations, conditions=None, **kwargs):
     #     bounds = MAZE_BOUNDS[self.env_name]
 

diffuser/utils/training.py
@@ -12,6 +12,8 @@ from .arrays import apply_dict, batch_to_device, to_device, to_np
 from .cloud import sync_logs
 from .timer import Timer
 
+from diffuser.datasets.d4rl import load_environment
+
 
 def cycle(dl):
     while True:
@@ -48,6 +50,7 @@ class Trainer(object):
         dataset,
         renderer,
         device,
+        name,
         ema_decay=0.995,
         train_batch_size=32,
         train_lr=2e-5,
@@ -69,7 +72,7 @@ class Trainer(object):
         self.ema = EMA(ema_decay)
         self.ema_model = copy.deepcopy(self.model)
         self.update_ema_every = update_ema_every
-
+        self.name =name
         self.step_start_ema = step_start_ema
         self.log_freq = log_freq
         self.sample_freq = sample_freq
@@ -183,12 +186,17 @@ class Trainer(object):
         if self.bucket is not None:
             sync_logs(self.logdir, bucket=self.bucket, background=self.save_parallel)
 
-    def load(self, epoch):
+    def load(self, epoch, directory = None):
         """
         loads model and ema from disk
         """
-        loadpath = os.path.join(self.logdir, f"state_{epoch}.pt")
-        data = torch.load(loadpath)
+        if directory is not None:
+            direc = directory
+        else:
+            direc = os.path.join(direc, f"state_{epoch}.pt")
+
+        loadpath = direc
+        data = torch.load(loadpath, map_location=torch.device('cpu'))
 
         self.step = data["step"]
         self.model.load_state_dict(data["model"])
@@ -239,15 +247,40 @@ class Trainer(object):
         savepath = os.path.join(self.logdir, f"_sample-reference.png")
         self.renderer.composite(savepath, observations)
 
-    def render_samples(self, batch_size=1, n_samples=1):
+    def render_samples(self, batch_size=1, n_samples=1, get_cond_from_env = False):
         """
         renders samples from (ema) diffusion model
+        Note: set get_cond_from_env to True if you want to get the conditions from the environment
         """
         for i in range(batch_size):
 
+            """modification beste"""
+            # print(batch.conditions)
+            # {0: tensor([[-0.5193,  0.4605, -0.1075, -0.0416]]), 255: tensor([[ 0.1836, -0.5136,  0.0587,  0.5159]])}
+
+
             ## get a single datapoint
-            batch = self.dataloader_vis.__next__()
-            conditions = to_device(batch.conditions, self.device)
+            if get_cond_from_env:
+                zeros = torch.zeros((1, 2))
+                env = load_environment(self.name)
+                conditions = {}
+                conditions[255] = torch.cat((torch.tensor(env.unwrapped.goal).reshape(1,-1), zeros), dim=0).reshape(1,-1)
+                print("Goal used in conditioning: ", conditions[255])
+                conditions[0] = torch.cat((torch.tensor(env.unwrapped.point_env.init_qpos[:2]).reshape(1,-1), zeros), dim=0).reshape(1,-1)
+               
+                normed_c = conditions[0]
+                print("Starting used in conditioning: ", conditions[0])
+                conditions = to_device(conditions, self.device)
+                print(conditions)
+            else:
+                batch = self.dataloader_vis.__next__()
+                conditions = to_device(batch.conditions, self.device)
+                normed_c = batch.conditions[0]
+
+            
+
+
+
             ## repeat each item in conditions `n_samples` times
             conditions = apply_dict(
                 einops.repeat,
@@ -266,7 +299,7 @@ class Trainer(object):
             normed_observations = samples[:, :, self.dataset.action_dim :]
 
             # [ 1 x 1 x observation_dim ]
-            normed_conditions = to_np(batch.conditions[0])[:, None]
+            normed_conditions = to_np(normed_c)[:, None]
 
             # from diffusion.datasets.preprocessing import blocks_cumsum_quat
             # observations = conditions + blocks_cumsum_quat(deltas)
@@ -290,3 +323,80 @@ class Trainer(object):
 
             savepath = os.path.join(self.logdir, f"sample-{self.step}-{i}.png")
             self.renderer.composite(savepath, observations)
+            
+
+    def render_samples_env(self, env, batch_size=1, n_samples=1, get_cond_from_env = False):
+        """
+        renders samples from (ema) diffusion model
+        Note: set get_cond_from_env to True if you want to get the conditions from the environment
+        """
+        for i in range(batch_size):
+
+            """modification beste"""
+            # print(batch.conditions)
+            # {0: tensor([[-0.5193,  0.4605, -0.1075, -0.0416]]), 255: tensor([[ 0.1836, -0.5136,  0.0587,  0.5159]])}
+
+
+            ## get a single datapoint
+            if get_cond_from_env:
+                zeros = torch.zeros((1, 2))
+                conditions = {}
+                conditions[255] = torch.cat((torch.tensor(env.unwrapped.goal).reshape(1,-1), zeros), dim=0).reshape(1,-1)
+                print("Goal used in conditioning: ", conditions[255])
+                conditions[0] = torch.cat((torch.tensor(env.unwrapped.point_env.init_qpos[:2]).reshape(1,-1), zeros), dim=0).reshape(1,-1)
+               
+                normed_c = conditions[0]
+                print("Starting used in conditioning: ", conditions[0])
+                conditions = to_device(conditions, self.device)
+                
+            else:
+                batch = self.dataloader_vis.__next__()
+                conditions = to_device(batch.conditions, self.device)
+                normed_c = batch.conditions[0]
+
+            
+
+
+
+            ## repeat each item in conditions `n_samples` times
+            conditions = apply_dict(
+                einops.repeat,
+                conditions,
+                "b d -> (repeat b) d",
+                repeat=n_samples,
+            )
+
+            ## [ n_samples x horizon x (action_dim + observation_dim) ]
+            samples = self.ema_model.conditional_sample(conditions)
+            samples = to_np(samples)
+
+            # print(f"Samples:{samples}")
+
+            ## [ n_samples x horizon x observation_dim ]
+            normed_observations = samples[:, :, self.dataset.action_dim :]
+
+            # [ 1 x 1 x observation_dim ]
+            normed_conditions = to_np(normed_c)[:, None]
+
+            # from diffusion.datasets.preprocessing import blocks_cumsum_quat
+            # observations = conditions + blocks_cumsum_quat(deltas)
+            # observations = conditions + deltas.cumsum(axis=1)
+
+            ## [ n_samples x (horizon + 1) x observation_dim ]
+            normed_observations = np.concatenate(
+                [np.repeat(normed_conditions, n_samples, axis=0), normed_observations],
+                axis=1,
+            )
+
+            ## [ n_samples x (horizon + 1) x observation_dim ]
+            observations = self.dataset.normalizer.unnormalize(
+                normed_observations, "observations"
+            )
+
+            #### @TODO: remove block-stacking specific stuff
+            # from diffusion.datasets.preprocessing import blocks_euler_to_quat, blocks_add_kuka
+            # observations = blocks_add_kuka(observations)
+            ####
+
+            savepath = os.path.join(self.logdir, f"sample-{self.step}-{i}.png")
+            self.renderer.composite(savepath, observations, env = env)