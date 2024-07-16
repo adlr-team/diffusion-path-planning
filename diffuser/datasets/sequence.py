fixed normalization error

diffuser/datasets/sequence.py
@@ -1,5 +1,6 @@
 import pdb
 from collections import namedtuple
+from multiprocessing import Pool
 
 import numpy as np
 import torch
@@ -14,7 +15,6 @@ from .buffer import ReplayBuffer
 from .d4rl import load_environment, sequence_dataset
 from .normalization import DatasetNormalizer, LimitsNormalizer
 from .preprocessing import get_preprocess_fn
-from multiprocessing import Pool
 
 Batch = namedtuple("Batch", "trajectories conditions")
 ValueBatch = namedtuple("ValueBatch", "trajectories conditions values")
@@ -213,7 +213,7 @@ class TrajectoryDataset(torch.utils.data.Dataset):
         self.dataset = self._batch_process(dataset, num_workers)
         self.action_dim = (7,)
         self.normalizer = LimitsNormalizer(
-            self.dataset
+            interpolate_trajectories(dataset, horizon)
         )  # Store normalizer for later use
 
     def _batch_process(self, dataset, num_workers):

justin_arm/training_justin.py
@@ -215,7 +215,7 @@ class Justin_Trainer(object):
                     f"{self.step}: {loss.item():8.4f} | {infos_str} | t: {timer():8.4f}"
                 )
 
-                self.step += 1
+            self.step += 1
 
         self.model.eval()
         val_loss = 0.0
@@ -425,9 +425,16 @@ class Justin_Trainer(object):
             "b d -> (repeat b) d",
             repeat=n_samples,
         )
+        # Iterate over the keys in the conditions dictionary
+        for key in conditions.keys():
+            conditions[key] = conditions[key] + 0.001 * torch.randn_like(
+                conditions[key]
+            )
+        # Add very msall perturbation to the conditions
 
         ## [ n_samples x horizon x (action_dim + observation_dim) ]
-        samples = self.model.conditional_sample(conditions)
+        print(conditions[0].shape)
+        samples = self.ema_model.conditional_sample(conditions)
         samples = to_np(samples)
 
         ## [ n_samples x horizon x observation_dim ]